743ebe5e3fd858335b937971bf72823ece2100e4
Rafaellincon2022 <rafaellincon123@gmail.com>
2023-05-02 16:47:50 -0300

    Exercícios Resolvidos
    Exercícios Resolvidos

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -1,1 +1,31 @@
-alert('Olá, estamos apenas testando!')
+function contar() {
+    var inicio = window.document.getElementById('txtinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpasso')
+    let resultado = window.document.getElementById('resultado')
+
+    if (inicio.value.length == 0 || fim.value.length== 0 || passo.value.length == 0) {
+        resultado.innerHTML = '<p> Impossível Contar! </p>'
+    } else {
+        resultado.innerHTML = `Contando: </br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido. Considerando PASSO = 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem Crescente
+            for (let contador = i; contador <= f; contador += p) {
+                resultado.innerHTML += `${contador} \u{1F449} `
+            }
+        } else {
+            // Contagem Regressiva
+            for (let contador = i; contador >= f; contador -= p) {
+                resultado.innerHTML += `${contador} \u{1F449} `
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+    }
+}
--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá, estamos apenas testando!')
+function tabuada() {
+    var numero = window.document.getElementById('txtnum') // Recebe o valor digitado
+    var caixa = window.document.getElementById('seltab') // Marca o local que será apresentado o resultado
+    // Condição para caso o campo esteja vazio
+    if (numero.value.length == 0) { 
+        window.alert('Por favor, digite um número!')
+        // Condição para caso o campo tenha valor informado
+    } else {
+        let n = Number(numero.value) // Transforma o valor em número
+        var contador = 1
+        caixa.innerHTML = '' // Limpa a caixa de resultado
+        while (contador <= 10) {
+            let item = document.createElement('option') // Cria as opções (cada linha)
+            item.text = `${n} X ${contador} = ${n * contador}` // Faz a tabuada
+            caixa.value = `caixa${contador}` // Os OPTIONs precisam ter values, então aqui fazemos isso
+            caixa.appendChild(item) // Adicionamos um elemento filho
+            contador ++ // Acrescenta mais um ao contador
+        }
+    }
+}
